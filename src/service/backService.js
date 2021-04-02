6779c1d960199c02d07d1af81498f6bc67b91ae3
juanmd9 <juanmmd9@gmail.com>
2021-04-02 10:26:12 -0500

    service para el back

---
+++ src/service/backService.js
@@ -0,0 +1,40 @@
+import axios from 'axios';
+
+const url = 'http://localhost:8080/';
+// const url = '';
+
+function servicePost(path, body){
+    console.log(path, body);
+    axios.post(url+path, 
+             body,
+            //  { headers: {
+            //     Authorization: localStorage.getItem("token"),
+            //   }}
+         )
+             .then(function (response) {
+                 alert("Success!");
+                 console.log(response.data);
+             })
+             .catch(function (error) {
+                 console.log(error);
+             });
+}
+
+function serviceGet(path, body){
+    console.log(path, body);
+    axios.get(url+path, 
+             body,
+            //  { headers: {
+            //     Authorization: localStorage.getItem("token"),
+            //   }}
+         )
+             .then(function (response) {
+                alert("Success!");
+                 console.log(response.data);
+             })
+             .catch(function (error) {
+                 console.log(error);
+             });
+}
+
+export {servicePost, serviceGet} ;
